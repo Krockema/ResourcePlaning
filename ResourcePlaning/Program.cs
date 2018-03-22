b4b47aaa5e53c65169aeec57f924f95be9a76092
Krockema <martin.krockert@gmail.com>
2018-03-22 12:29:49 +0100

    Inital Comit

--- ResourcePlaning/Program.cs
+++ ResourcePlaning/Program.cs
@@ -6,7 +6,7 @@ namespace ResourcePlaning
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Tesst");
         }
     }
 }
